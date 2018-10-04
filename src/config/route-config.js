fb3444ec4728a92406fa812989f8685972095d56
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-10-04 14:38:23 -0500

    introduction to express assignment

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   init(app){
-    const staticRoutes = require("../routes/static");
-    app.use(staticRoutes);
+    const marcoRoutes = require("../routes/marco");
+    app.use(marcoRoutes);
   }
 }
