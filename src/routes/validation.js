f881d78c4e81bd008e2a5bc4e583d2f3991c5f1e
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-10-28 12:57:13 -0500

    october 28th

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -22,27 +22,45 @@ module.exports = {
       return next();
     }
   },
+  validateUsers(req, res, next) {
+      if(req.method === "POST") {
+
+ // #1
+        req.checkBody("email", "must be valid").isEmail();
+        req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+        req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+      }
+
+      const errors = req.validationErrors();
+
+      if (errors) {
+        req.flash("error", errors);
+        return res.redirect(req.headers.referer);
+      } else {
+        return next();
+      }
+    },
 
   validateTopics(req, res, next) {
 
 //#1
-    if(req.method === "POST") {
+   if(req.method === "POST") {
 
 //#2
-      req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
-      req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
-    }
+     req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
+     req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
+   }
 
 //#3
-    const errors = req.validationErrors();
+   const errors = req.validationErrors();
 
-    if (errors) {
+   if (errors) {
 
 //#4
-      req.flash("error", errors);
-      return res.redirect(303, req.headers.referer)
-    } else {
-      return next();
-    }
-  }
+     req.flash("error", errors);
+     return res.redirect(303, req.headers.referer)
+   } else {
+     return next();
+   }
+ }
 }
