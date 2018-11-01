d28aa8daec7e260f42ef98e36ce4ab0ac9432f41
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-10-31 19:20:08 -0500

    updates oct 31

--- src/db/seeds/20181031184006-topics.js
+++ src/db/seeds/20181031184006-topics.js
@@ -1,42 +1,42 @@
-'use strict';
+ "use strict";
 
 const faker = require("faker");
 
 //#2
- let topics = [];
+let topics = [];
 
- for(let i = 1 ; i <= 15 ; i++){
-   topics.push({
-     title: faker.hacker.noun(),
-     description: faker.hacker.phrase(),
-     createdAt: new Date(),
-     updatedAt: new Date()
-   });
-
+for(let i = 1 ; i <= 15 ; i++){
+  topics.push({
+    title: faker.hacker.noun(),
+    description: faker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Topics", topics, {});
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+   up: (queryInterface, Sequelize) => {
+     /*
+       Add altering commands here.
+       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+       Example:
+       return queryInterface.bulkInsert("Person", [{
+         name: "John Doe",
+         isBetaMember: false
+       }], {});
+     */
+     return queryInterface.bulkInsert("Topics", topics, {});
+   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+   down: (queryInterface, Sequelize) => {
+     /*
+       Add reverting commands here.
+       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-    return queryInterface.bulkDelete("Topics", null, {});
-  }
-};
+       Example:
+       return queryInterface.bulkDelete("Person", null, {});
+     */
+     return queryInterface.bulkDelete("Topics", null, {});
+   }
+ };
--- src/db/seeds/20181031184012-posts.js
+++ src/db/seeds/20181031184012-posts.js
@@ -8,7 +8,7 @@ const faker = require("faker");
  for(let i = 1 ; i <= 15 ; i++){
    posts.push({
      title: faker.hacker.noun(),
-     description: faker.hacker.phrase(),
+     body: faker.hacker.phrase(),
      createdAt: new Date(),
      updatedAt: new Date()
    });
